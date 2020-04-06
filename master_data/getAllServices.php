2367baadb7368f5c8b7134c10215725a6aa8d895
xieTY <xieTY@xieTYs-MacBook-Air.local>
2020-04-06 22:47:25 +0800

    fix

--- master_data/getAllServices.php
+++ master_data/getAllServices.php
@@ -2,17 +2,27 @@
 
 	require '../api_conf.php';
 	
-	$data = $dale->kueri("SELECT * FROM `master_services` ORDER BY `services_name` ASC");
+	$data = "";
+
+	if(isset($_GET['id'])){
+		$data = $dale->kueri("SELECT * FROM `master_services` WHERE `services_id` = '".$_GET['id']."'");
+	}
+	else{
+		$data = $dale->kueri("SELECT * FROM `master_services` ORDER BY `services_name` ASC");
+	}
+
 	
 	$data = json_decode($data);
 	$json_data = [];
 
 	for($i = 0; $i < sizeof($data); $i++){
 
-		$json_data[$i][0]['data']  = $i + 1;
+		$json_data[$i][0]['data']  = $data[$i] -> services_id;
+		$json_data[$i][0]['type']  = "id";
 
 		// services name
 		$json_data[$i][1]['data']  = $data[$i] -> services_name;
+		$json_data[$i][1]['type']  = "text";
 		$json_data[$i][1]['class'] = "";
 
 		// services price
@@ -24,7 +34,7 @@
 		$json_data[$i][3]['data']  = $data[$i] -> services_category;
 		$json_data[$i][3]['type']  = "badge";
 
-		if($data[$i] -> services_category == "Clinic"){
+		if($data[$i] -> services_category == "Klinik"){
 			$json_data[$i][3]['class'] = "badge badge-primary";
 		}
 		else{
--- master_data/insertEditServices.php
+++ master_data/insertEditServices.php
@@ -1,22 +1,30 @@
 <?php
 	
 	require '../api_conf.php';
-	
+
+
 	if(isset($_GET['data_0'])){
+
+		$id = $_GET['data_id'];
+
+		if($id == null){
+			$id = md5($id);
+		}
+
 		$data = $dale->kueri("INSERT INTO `master_services` 
-						 SET services_id       = '".md5($_GET['data_0'])."',
-							 services_name     = '".$_GET['data_0']."',
-							 services_category = '".$_GET['data_2']."',
-							 services_price    = '".$_GET['data_1']."',
-							 created_by        = 'NULL',
-							 updated_by        = 'NULL' 
-					     ON DUPLICATE KEY
-					         services_id       = '".md5($_GET['data_0'])."',
-							 services_name     = '".$_GET['data_0']."',
-							 services_category = '".$_GET['data_2']."',
-							 services_price    = '".$_GET['data_1']."',
-							 created_by        = 'NULL',
-							 updated_by        = 'NULL' ");
+							  SET services_id        = '".$id."', 
+							  	  services_name      = '".$_GET['data_0']."', 
+							  	  services_category  = '".$_GET['data_2']."',
+							  	  services_price     = '".$_GET['data_1']."', 
+							  	  created_by         = 'NULL', 
+							  	  updated_by         = 'NULL' 
+							  ON DUPLICATE KEY UPDATE 
+							  	  services_name      = '".$_GET['data_0']."', 
+							  	  services_category  = '".$_GET['data_2']."',
+							  	  services_price     = '".$_GET['data_1']."', 
+							  	  created_by         = 'NULL', 
+							  	  updated_by         = 'NULL'");
+		echo $data;
 	}
 	
 ?>
